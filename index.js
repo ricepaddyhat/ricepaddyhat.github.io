a0439aa339e73494759f20a5289e700dc31a384e
Matt <36835759+ricepaddyhat@users.noreply.github.com>
2019-05-18 14:00:11 +1000

    fixed tab bugs, tidied code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-function openTab(event, tabName) {
-	var i, tableContent, tabButtons;
-	
+function openTab(evt, tabName) {
+	var i, tabContent, tabButtons;
 	tabContent = document.getElementsByClassName("tabContent");
 
-	for(i=0; i < tabContent.length; i++) {
+	for(i = 0; i < tabContent.length; i++) {
 		tabContent[i].style.display = "none";
 	}
 
-  	tabButtons = document.getElementsByClassName("tabButtons");
-  	for (i = 0; i < tabButtons.length; i++) {
-    	tabButtons[i].className = tabButtons[i].className.replace(" active", "");
-  	}
+  tabButtons = document.getElementsByClassName("tabButtons");
+  for(i = 0; i < tabButtons.length; i++) {
+   	tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+  }
 
-  	document.getElementById(tabName).style.display = "block";
-  	evt.currentTarget.className += " active";
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 
 }
